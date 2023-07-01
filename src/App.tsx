bfd2bbf3c52d62f6be6390050bc58be64f144900
Leandro Campos <camposweb@gmail.com>
2023-07-01 20:24:33 -0300

    install TailwindCSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,11 @@
+import { Provider as ReduxProvider } from 'react-redux'
+import { store } from './store'
+import { Player } from './pages/Player'
+
 export function App() {
-  return <></>
+  return (
+    <ReduxProvider store={store}>
+      <Player />
+    </ReduxProvider>
+  )
 }
